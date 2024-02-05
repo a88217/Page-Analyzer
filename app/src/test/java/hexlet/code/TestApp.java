60010a250de4e3a19255b54ae45e7fe6b0f24d30
aleksandr muzalev <muzalev.as@gmail.com>
2024-02-05 11:45:04 +0300

    Preparation 4

---
+++ app/src/test/java/hexlet/code/TestApp.java
@@ -0,0 +1,12 @@
+package hexlet.code;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class TestApp {
+    @Test
+    public void testApp() throws Exception {
+        boolean expected = true;
+        boolean actual = true;
+        assertEquals(expected, actual);
+    }
+}
