7fd1236dfe089dd8493e0f8dd29d4d5eab2843cb
aleksandr muzalev <muzalev.as@gmail.com>
2024-02-05 13:55:45 +0300

    Step 3.1

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,9 +1,15 @@
 package hexlet.code;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import hexlet.code.repository.BaseRepository;
 import io.javalin.Javalin;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.sql.SQLException;
+import java.util.stream.Collectors;
 
 public class App {
 
@@ -13,12 +19,35 @@ public class App {
         return Integer.valueOf(port);
     }
 
+    private static String getDB() {
+        String database = System.getenv().getOrDefault("JDBC_DATABASE_URL", "jdbc:h2:mem:project;DB_CLOSE_DELAY=-1;");
+        return database;
+    }
+
     public static void main(String[] args) throws IOException, SQLException {
         var app = getApp();
         app.start(getPort());
     }
 
     public static Javalin getApp() throws IOException, SQLException {
+
+        var hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(getDB());
+
+        var dataSource = new HikariDataSource(hikariConfig);
+
+        // Получаем путь до файла в src/main/resources
+        var url = App.class.getClassLoader().getResource("schema.sql");
+        var file = new File(url.getFile());
+        var sql = Files.lines(file.toPath())
+                .collect(Collectors.joining("\n"));
+
+        // Получаем соединение, создаем стейтмент и выполняем запрос
+        try (var connection = dataSource.getConnection();
+             var statement = connection.createStatement()) {
+            statement.execute(sql);
+        }
+        BaseRepository.dataSource = dataSource;
 
         var app = Javalin.create(config -> {
             config.plugins.enableDevLogging();
---
+++ app/src/main/java/hexlet/code/model/Url.java
@@ -0,0 +1,20 @@
+package hexlet.code.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+public class Url {
+    private long id;
+
+    private String name;
+    private Timestamp createdAt;
+
+    public Url(String name, Timestamp createdAt) {
+        this.name = name;
+        this.createdAt = createdAt;
+    }
+}
