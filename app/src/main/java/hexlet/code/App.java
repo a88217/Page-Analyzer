2195ce8a145b317754d2a0b042f5e64c37c576fd
aleksandr muzalev <muzalev.as@gmail.com>
2024-02-08 14:58:53 +0300

    Step 3.6

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -6,13 +6,18 @@ import hexlet.code.repository.BaseRepository;
 import hexlet.code.utils.DatabaseConfig;
 import io.javalin.Javalin;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.sql.SQLException;
 import java.util.stream.Collectors;
 
 public class App {
+
+    private static final String SCHEMA_FILE = "schema.sql";
 
     private static int getPort() {
         String port = System.getenv().getOrDefault("PORT", "7070");
@@ -23,6 +28,13 @@ public class App {
     public static void main(String[] args) throws IOException, SQLException {
         var app = getApp();
         app.start(getPort());
+    }
+
+    private static String readResourceFile(String fileName) throws IOException {
+        var inputStream = App.class.getClassLoader().getResourceAsStream(fileName);
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+            return reader.lines().collect(Collectors.joining("\n"));
+        }
     }
 
     public static Javalin getApp() throws IOException, SQLException {
@@ -47,16 +59,23 @@ public class App {
         var dataSource = new HikariDataSource(hikariConfig);
 
         // Получаем путь до файла в src/main/resources
-        var url = App.class.getClassLoader().getResource("schema.sql");
-        var file = new File(url.getFile());
-        var sql = Files.lines(file.toPath())
-                .collect(Collectors.joining("\n"));
+//        var url = App.class.getClassLoader().getResource("schema.sql");
+//        var file = new File(url.getFile());
+//        var sql = Files.lines(file.toPath())
+//                .collect(Collectors.joining("\n"));
+//
+//        // Получаем соединение, создаем стейтмент и выполняем запрос
+//        try (var connection = dataSource.getConnection();
+//             var statement = connection.createStatement()) {
+//            statement.execute(sql);
+//        }
+        var sql = readResourceFile(SCHEMA_FILE);
 
-        // Получаем соединение, создаем стейтмент и выполняем запрос
         try (var connection = dataSource.getConnection();
              var statement = connection.createStatement()) {
             statement.execute(sql);
         }
+
         BaseRepository.dataSource = dataSource;
 
         var app = Javalin.create(config -> {
