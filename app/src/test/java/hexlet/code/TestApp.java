e12a5e9e8185776150c898a5b08512dbffae7214
aleksandr muzalev <muzalev.as@gmail.com>
2024-02-12 18:42:31 +0300

    Step 5.5

--- app/src/test/java/hexlet/code/TestApp.java
+++ app/src/test/java/hexlet/code/TestApp.java
@@ -1,12 +1,42 @@
 package hexlet.code;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import hexlet.code.utils.NamedRoutes;
+import io.javalin.Javalin;
+import io.javalin.testtools.JavalinTest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+import java.sql.SQLException;
 
 public class TestApp {
-    @Test
-    public void testApp() throws Exception {
-        boolean expected = true;
-        boolean actual = true;
-        assertEquals(expected, actual);
+
+    Javalin app;
+
+    @BeforeEach
+    public final void setUp() throws IOException, SQLException {
+        app = App.getApp();
     }
+
+    @Test
+    public void testRootPage() {
+        JavalinTest.test(app, (server, client) -> {
+            var response = client.get(NamedRoutes.rootPath());
+            assertThat(response.code()).isEqualTo(200);
+            assertThat(response.body().string()).contains(
+                    "Анализатор страниц");
+        });
+    }
+
+    @Test
+    public void testUrlsPage() {
+        JavalinTest.test(app, (server, client) -> {
+            var response = client.get(NamedRoutes.urlsPath());
+            assertThat(response.code()).isEqualTo(200);
+            assertThat(response.body().string()).contains("Сайты");
+        });
+    }
+
 }
